e4a7639d74691d885a4b3fd0831063179a93683e
DAZ <daz4126@gmail.com>
2011-07-29 19:42:28 +0100

    style changes and auto_migrate

--- main.rb
+++ main.rb
@@ -17,6 +17,7 @@ class List
   has n, :tasks, :constraint => :destroy  
 end
 DataMapper.finalize
+DataMapper.auto_migrate!
 get('/styles.css'){ content_type 'text/css', :charset => 'utf-8' ; scss :styles }
 get '/' do
   @lists = List.all(:order => [:name])
@@ -83,16 +84,17 @@ li.list
     input type="hidden" name="_method" value="DELETE"
     input type="submit" value="&times;"
 @@styles
+body{font:11px/1 Arial, Helvetica, sans-serif;}
 .completed{text-decoration: line-through;}
 .lists,.tasks{padding:0;list-style:none;overflow:hidden;}
 .list{width:18%;margin:10px 1%;float:left;position:relative;
-form.destroy{position:absolute;right:0;top:0;display:none;}
+form.destroy{position:absolute;right:0;top:0;display:none;margin:0;padding:0}
 &:hover form.destroy{display:block;}
-form.add{margin:0 auto;}
+form.new{input{width:80%;display:block;margin:0 auto;}}
 h2{text-align:center;margin:0;}}
 .task{overflow:hidden;border-bottom:dotted 1px #ccc;
-padding:0;position:relative;padding:4px 28px;
-form.update{position:absolute;bottom:4px;left:0;
+padding:0;position:relative;padding:2px 0 2px 28px;
+form.update{position:absolute;bottom:2px;left:0;
 input{background:white;color:white;padding:0 2px;border:solid 1px gray;cursor:pointer;}}
 &.completed form.update input{color:#000;}
 form.delete{display:inline;
